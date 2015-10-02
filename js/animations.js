40b36d257ac6735c4f6250563bbad80068211fc1
varusgarcia <Alvaro@alvaros-MacBook-Pro.local>
2015-10-02 16:49:42 +0200

    updated projects
    implemented tags function (show/hide)

--- js/animations.js
+++ js/animations.js
@@ -12,10 +12,37 @@ $("[rel='tooltip']").tooltip();
 
 $('.thumbnail').hover(
     function(){
-        $(this).find('.caption').fadeIn(250); //.fadeIn(250)
+        $(this).find('.caption').fadeIn(500);
     },
     function(){
-        $(this).find('.caption').fadeOut(250); //.fadeOut(205)
+        $(this).find('.caption').fadeOut(500);
     }
 );
+$("#showall_tag_search").click(function(e) {
+    e.preventDefault();
+    $(".tag_display").show();
+});
+$("#slider_tag_search").click(function(e) {
+    e.preventDefault();
+    $(".tag_display").hide();
+    $(".slider_tag").show();
+});
+$("#hyperlapse_tag_search").click(function(e) {
+    e.preventDefault();
+    $(".tag_display").hide();
+    $(".hyperlapse_tag").show();
+
+});
+$("#video_tag_search").click(function(e) {
+    e.preventDefault();
+    $(".tag_display").hide();
+    $(".video_tag").show();
+
+});
+$("#diy_tag_search").click(function(e) {
+    e.preventDefault();
+    $(".tag_display").hide();
+    $(".diy_tag").show();
+
+});
 });
